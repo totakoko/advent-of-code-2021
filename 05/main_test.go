2fad374cf110783cb7b05b4a959035fd7d9f2a5f
Maxime Dréau <maxime@dreau.fr>
2021-12-05 10:44:29 +0100

    Day 5: clean up test

--- 05/main_test.go
+++ 05/main_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Bingo(t *testing.T) {
+func Test_Day5(t *testing.T) {
 	input := strings.Split(`0,9 -> 5,9
 8,0 -> 0,8
 9,4 -> 3,4
@@ -18,18 +18,6 @@ func Test_Bingo(t *testing.T) {
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2`, "\n")
-	// 	diagram := `.......1..
-	// ..1....1..
-	// ..1....1..
-	// .......1..
-	// .112111211
-	// ..........
-	// ..........
-	// ..........
-	// ..........
-	// 222111....`
-	// require.Equal(t, diagram, getGridAsString(drawSegments(input, 10)))
-	// require.Equal(t, diagram, getGridAsString(drawSegments(input, 10)))
 	require.Equal(t, 5, countPart1Overlaps(input, 10))
 	require.Equal(t, 12, countPart2Overlaps(input, 10))
 }
