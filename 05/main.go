8850fa339408d812b2823cb5ae5c443a907e7bf6
Maxime Dréau <maxime@dreau.fr>
2021-12-05 10:37:26 +0100

    Day 5

---
+++ 05/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	content, _ := ioutil.ReadFile("input.txt")
+	input := strings.Split(string(content), "\n")
+	input = input[:len(input)-1]
+
+	fmt.Println("Part 1:", countPart1Overlaps(input, 1000))
+	fmt.Println("Part 2:", countPart2Overlaps(input, 1000))
+}
+
+// part 1
+func countPart1Overlaps(segments []string, gridSize int) int {
+	// for each line, extract segments
+	// draw in the matrix
+	grid := make([]int, gridSize*gridSize)
+	for _, segment := range segments {
+		parts := strings.Split(segment, " ")
+		startCoordinatesStr := strings.Split(parts[0], ",")
+		// omit the middle arrow
+		endCoordinatesStr := strings.Split(parts[2], ",")
+		x1, _ := strconv.Atoi(startCoordinatesStr[0])
+		y1, _ := strconv.Atoi(startCoordinatesStr[1])
+		x2, _ := strconv.Atoi(endCoordinatesStr[0])
+		y2, _ := strconv.Atoi(endCoordinatesStr[1])
+
+		// swap values to ensure an ascending order
+
+		// only draw horizontal or vertical lines
+		if x1 == x2 {
+			if y1 > y2 {
+				y1, y2 = y2, y1
+			}
+			for y := y1; y <= y2; y++ {
+				grid[y*gridSize+x1]++
+			}
+		}
+		if y1 == y2 {
+			if x1 > x2 {
+				x1, x2 = x2, x1
+			}
+			for x := x1; x <= x2; x++ {
+				grid[y1*gridSize+x]++
+			}
+		}
+	}
+
+	// count overlaps
+	overlapsCount := 0
+	for _, n := range grid {
+		if n >= 2 {
+			overlapsCount++
+		}
+	}
+	return overlapsCount
+}
+
+// part 2
+func countPart2Overlaps(segments []string, gridSize int) int {
+	// for each line, extract segments
+	// draw in the matrix
+	grid := make([]int, gridSize*gridSize)
+	for _, segment := range segments {
+		parts := strings.Split(segment, " ")
+		startCoordinatesStr := strings.Split(parts[0], ",")
+		// omit the middle arrow
+		endCoordinatesStr := strings.Split(parts[2], ",")
+		x1, _ := strconv.Atoi(startCoordinatesStr[0])
+		y1, _ := strconv.Atoi(startCoordinatesStr[1])
+		x2, _ := strconv.Atoi(endCoordinatesStr[0])
+		y2, _ := strconv.Atoi(endCoordinatesStr[1])
+
+		// only draw horizontal or vertical lines
+		switch true {
+		case x1 == x2:
+			if y1 > y2 {
+				y1, y2 = y2, y1
+			}
+			for y := y1; y <= y2; y++ {
+				grid[y*gridSize+x1]++
+			}
+		case y1 == y2:
+			if x1 > x2 {
+				x1, x2 = x2, x1
+			}
+			for x := x1; x <= x2; x++ {
+				grid[y1*gridSize+x]++
+			}
+		default:
+			// swap values to ensure an ascending order
+			if x1 > x2 {
+				x1, x2 = x2, x1
+				y1, y2 = y2, y1
+			}
+
+			// diagonal lines are at 45°
+			if y1 < y2 {
+				for i := 0; i <= x2-x1; i++ {
+					grid[(y1+i)*gridSize+(x1+i)]++
+				}
+			} else {
+				for i := 0; i <= x2-x1; i++ {
+					grid[(y1-i)*gridSize+(x1+i)]++
+				}
+			}
+		}
+	}
+
+	// count overlaps
+	overlapsCount := 0
+	for _, n := range grid {
+		if n >= 2 {
+			overlapsCount++
+		}
+	}
+	return overlapsCount
+}
---
+++ 05/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Bingo(t *testing.T) {
+	input := strings.Split(`0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`, "\n")
+	// 	diagram := `.......1..
+	// ..1....1..
+	// ..1....1..
+	// .......1..
+	// .112111211
+	// ..........
+	// ..........
+	// ..........
+	// ..........
+	// 222111....`
+	// require.Equal(t, diagram, getGridAsString(drawSegments(input, 10)))
+	// require.Equal(t, diagram, getGridAsString(drawSegments(input, 10)))
+	require.Equal(t, 5, countPart1Overlaps(input, 10))
+	require.Equal(t, 12, countPart2Overlaps(input, 10))
+}
