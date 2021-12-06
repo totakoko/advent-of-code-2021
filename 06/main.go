199db4a203d2470dbf0e7cabaf0c32f24e57a626
Maxime Dréau <maxime@dreau.fr>
2021-12-06 10:08:11 +0100

    Day 06: add big.Int implementation

--- 06/main.go
+++ 06/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -13,6 +14,10 @@ func main() {
 
 	fmt.Println("# Population after 80 days", evolvePopulation(parseInput(input[0]), 80))
 	fmt.Println("# Population after 256 days", evolvePopulationWithModel(parseInput(input[0]), 256))
+
+	fmt.Println("Bonus:")
+	fmt.Println("# Population after 1000 days", evolveBigPopulationWithModel(parseInput(input[0]), 1000))
+	fmt.Println("# Population after 10000 days", evolveBigPopulationWithModel(parseInput(input[0]), 10000))
 }
 
 func parseInput(input string) []int {
@@ -54,6 +59,24 @@ func evolvePopulationWithModel(population []int, days int) int {
 	totalPopulation := 0
 	for _, populationSize := range populationByState {
 		totalPopulation += populationSize
+	}
+	return totalPopulation
+}
+
+// big.Int implementation, if we want lots of days
+func evolveBigPopulationWithModel(population []int, days int) *big.Int {
+	populationByState := make([]big.Int, 9)
+	for _, fishState := range population {
+		populationByState[fishState] = *big.NewInt(int64(fishState))
+	}
+
+	for day := 0; day < days; day++ {
+		populationByState = append(populationByState[1:], populationByState[0])
+		populationByState[6].Add(&populationByState[6], &populationByState[8])
+	}
+	totalPopulation := big.NewInt(0)
+	for _, populationSize := range populationByState {
+		totalPopulation.Add(totalPopulation, &populationSize)
 	}
 	return totalPopulation
 }
