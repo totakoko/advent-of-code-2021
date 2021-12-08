912d0b2bc509fc17c04a47cee7b6e21539170aa1
Maxime Dréau <maxime@dreau.fr>
2021-12-08 09:33:01 +0100

    Update template

--- _template/main.go
+++ _template/main.go
@@ -11,9 +11,14 @@ func main() {
 	input := strings.Split(string(content), "\n")
 	input = input[:len(input)-1]
 
-	fmt.Println("# Result", getResult(input))
+	fmt.Println("# Part 1", part1(input))
+	fmt.Println("# Part 2", part2(input))
 }
 
-func getResult(input []string) int {
+func part1(input []string) int {
+	return 0
+}
+
+func part2(input []string) int {
 	return 0
 }
--- _template/main_test.go
+++ _template/main_test.go
@@ -9,5 +9,6 @@ import (
 
 func Test_Result(t *testing.T) {
 	input := strings.Split(`1 2 3`, "\n")
-	require.Equal(t, 0, getResult(input))
+	require.Equal(t, 0, part1(input))
+	require.Equal(t, 0, part2(input))
 }
