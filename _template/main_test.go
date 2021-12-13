af417b04ef2579e2878fdc43240021f0461b1ea5
Maxime Dréau <maxime@dreau.fr>
2021-12-13 08:21:50 +0100

    Update template: use 2 distinct tests

--- _template/main_test.go
+++ _template/main_test.go
@@ -7,8 +7,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Result(t *testing.T) {
-	input := strings.Split(`1 2 3`, "\n")
+const inputText = `
+
+`
+
+func Test_Part1(t *testing.T) {
+	input := strings.Split(strings.TrimSpace(inputText), "\n")
 	require.Equal(t, 0, part1(input))
+}
+
+func Test_Part2(t *testing.T) {
+	input := strings.Split(strings.TrimSpace(inputText), "\n")
 	require.Equal(t, 0, part2(input))
 }
